7c5f4fa17a0d9e2986fa40ecdcbf583923289fdf
FoxKnight679 <felipewerncke17@gmail.com>
2026-02-24 22:56:46 -0300

    TrocaDePaginas

--- InventárioD&D/FrontEnd/JS/app.js
+++ InventárioD&D/FrontEnd/JS/app.js
@@ -1,1 +1,20 @@
-angular.module('DMShield', []);
+angular.module('DMShield', ['ngRoute'])
+.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $routeProvider
+    $locationProvider.hashPrefix('');
+    
+    $routeProvider
+    .when('/',    { templateUrl: 'Views/home.html' })
+    .when('/pagina1', { templateUrl: 'Views/pagina1.html' })
+    .when('/pagina2', { templateUrl: 'Views/pagina2.html' })
+    .when('/pagina3', { templateUrl: 'Views/pagina3.html' })
+    .otherwise({ redirectTo: '/' });
+}])
+.component('appHeader', {
+  templateUrl: 'Views/header.html',
+  controller: function(){ /* ... */ }
+})
+.component('appFooter', {
+  templateUrl: 'Views/footer.html',
+  controller: function(){ /* ... */ }
+});
